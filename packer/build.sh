f4932ac8db2dd567faacf6e8740bc681c55860bc
Jaime Jorge <jaime.jorge@scmspain.com>
2015-08-27 16:43:17 +0200

    packer/build.sh

--- packer/build.sh
+++ packer/build.sh
@@ -9,10 +9,10 @@ RND=$RANDOM
 rsync  -va  -arvce "ssh -i filea.txt -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no " .  packer@52.19.153.94:/tmp/$RND
 ssh -i filea.txt -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no packer@52.19.153.94 \
 "\
-export creator=$(git --no-pager show -s --format='%ae' ${TRAVIS_COMMIT}) \
-export creation_time=`date +"%Y%m%d%H%M%S"` \
-export appversion="0.0.${TRAVIS_BUILD_ID}" \
-export ec2_source_ami=ami-a10897d6 \
+export creator=$(git --no-pager show -s --format='%ae' ${TRAVIS_COMMIT}); \
+export creation_time=`date +"%Y%m%d%H%M%S"` ;\
+export appversion="0.0.${TRAVIS_BUILD_ID}"; \
+export ec2_source_ami=ami-a10897d6 ;\
 packer build /tmp/$RND/packer/build.json "
 
  
