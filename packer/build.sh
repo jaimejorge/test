300dfaaabb9174f80b42939357d9eb661a133f91
Jaime Jorge <jaime.jorge@scmspain.com>
2015-08-27 16:16:40 +0200

    packer/build.sh

--- packer/build.sh
+++ packer/build.sh
@@ -9,6 +9,7 @@ export creator=$(git --no-pager show -s --format='%ae' ${TRAVIS_COMMIT})
 export creation_time=`date +"%Y%m%d%H%M%S"`
 export appversion="0.0.${TRAVIS_BUILD_ID}"
 export ec2_source_ami=ami-a10897d6
+
 ssh -i filea.txt -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no packer@52.19.153.94 \
 "packer build packer/build.json"
  
