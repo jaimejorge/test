670c14133181a8ec1e2535a0a878bd640a702e6b
Jaime Jorge <jaime.jorge@scmspain.com>
2015-08-27 16:42:18 +0200

    packer/build.sh

--- packer/build.sh
+++ packer/build.sh
@@ -4,15 +4,17 @@ set -xe
 
 chmod 600 filea.txt
 
-export creator=$(git --no-pager show -s --format='%ae' ${TRAVIS_COMMIT})
-export creation_time=`date +"%Y%m%d%H%M%S"`
-export appversion="0.0.${TRAVIS_BUILD_ID}"
-export ec2_source_ami=ami-a10897d6
 
 RND=$RANDOM
 rsync  -va  -arvce "ssh -i filea.txt -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no " .  packer@52.19.153.94:/tmp/$RND
 ssh -i filea.txt -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no packer@52.19.153.94 \
-" packer build /tmp/$RND/packer/build.json ;"
+"\
+export creator=$(git --no-pager show -s --format='%ae' ${TRAVIS_COMMIT}) \
+export creation_time=`date +"%Y%m%d%H%M%S"` \
+export appversion="0.0.${TRAVIS_BUILD_ID}" \
+export ec2_source_ami=ami-a10897d6 \
+packer build /tmp/$RND/packer/build.json "
+
  
 #packer -machine-readable build    packer/build.json | tee output.txt
 #tail -2 output.txt | head -2 | awk 'match($0, /ami-.*/) { print substr($0, RSTART, RLENGTH) }' >  ami.txt
